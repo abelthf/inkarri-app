109e8295a4a6e516aec0d3ec52643652855abbc2
Fredy Abel Huanca Torres <abel.huanca@upeu.edu.pe>
2018-11-05 18:37:55 -0500

    travis

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -5,11 +5,12 @@ import os
 
 from flask import Flask  # <-- nuevo
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_debugtoolbar import DebugToolbarExtension
+from flask_cors import CORS
 
 # instanciado la db
 db = SQLAlchemy()
-
+toolbar = DebugToolbarExtension()
 
 # new
 def create_app(script_info=None):
@@ -17,12 +18,16 @@ def create_app(script_info=None):
     # instanciando la app
     app = Flask(__name__)
 
+    # habilitando CORS
+    CORS(app)
+
     # estableciendo configuracion
     app_settings = os.getenv('APP_SETTINGS')
     app.config.from_object(app_settings)
 
     # configurando extensiones
     db.init_app(app)
+    toolbar.init_app(app)
 
     # registro blueprints
     from project.api.users import users_blueprint
